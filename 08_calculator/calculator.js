fec33b5ac2d4e5c7fde2a43c93a46f09a861e64d
Timothy Winsley <t-winsley@protonmail.com>
2022-05-22 19:11:45 -0400

    Completed and verified passing tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function ([...args]) {
+  let sum = 0;
+  for (var i = 0; i < args.length; i++) {
+    sum += args[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function ([...args]) {
+  let total = args[0];
+  for (var i = 1; i < args.length; i++) {
+    total = total * args[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
