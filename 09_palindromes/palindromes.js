e23617962fab053a37cd9ec570f41cb4cfa186b4
Timothy Winsley <t-winsley@protonmail.com>
2022-05-22 19:35:19 -0400

    Complete and passes tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+  let input = a.toLowerCase();
+  input = input.replace(/[^a-zA-Z]/g, "");
+  if (input === input.split("").reverse().join("")) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
