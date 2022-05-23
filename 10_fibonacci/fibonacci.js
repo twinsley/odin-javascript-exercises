4966a5e0b2aa73dcf2ba3c4a061e5a63b6e8b668
Timothy Winsley <t-winsley@protonmail.com>
2022-05-22 20:35:14 -0400

    Complete and passes tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num === 1) {
+    return [1];
+  } else if (num < 0) {
+    return "OOPS";
+  } else {
+    let a = 1,
+      b = 0,
+      temp;
+    while (num >= 1) {
+      temp = a;
+      a = a + b;
+      b = temp;
+      num--;
+    }
 
+    return b;
+  }
 };
 
 // Do not edit below this line
