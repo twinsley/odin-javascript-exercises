19b5619dd088fc62d298b30bde4068426835977a
Timothy Winsley <t-winsley@protonmail.com>
2022-05-22 22:19:24 -0400

    Complete and pass test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let people = Array.from(arr);
+  let oldest = { name: "sample", yearOfBirth: 0, yearOfDeath: 1 };
+  let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].yearOfDeath === undefined) {
+      people[i].yearOfDeath = new Date().getFullYear();
+    }
+    let age = people[i].yearOfDeath - people[i].yearOfBirth;
+    if (age > oldestAge) {
+      oldest = people[i];
+      oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    }
+  }
+  return oldest;
 };
 
 // Do not edit below this line
