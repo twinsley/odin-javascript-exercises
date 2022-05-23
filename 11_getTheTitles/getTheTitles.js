66b4127bd1936b6f5607e785cba86446ac93ba00
Timothy Winsley <t-winsley@protonmail.com>
2022-05-22 21:16:25 -0400

    Completed and passed test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function ([...arr]) {
+  let items = [...arr];
+  let result = [];
+  for (let i = 0; i < items.length; i++) {
+    result.push(items[i].title);
+  }
+  return result;
 };
 
 // Do not edit below this line
